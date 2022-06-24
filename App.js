65f6e1c0278c8d447b64b5e312dcc1b9383faa9c
Rodolfo Gomez Sirimarco <im@rodolfo.gs>
2022-06-24 11:46:53 -0500

    Updated App.js

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import React, { useEffect, useRef } from 'react';
+import { View, Button } from 'react-native';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+let counter = 1;
+
+async function sendPushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: `Test notification number ${counter++}`,
+      body: 'Here is the notification body',
+      data: { data: 'goes here' },
+    },
+    trigger: { seconds: 1 },
+  });
+}
 
 export default function App() {
+  const responseListener = useRef();
+
+  useEffect(() => {
+    // This listen when you tap the notification and show the title in an alert
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      alert(response.notification.request.content.title);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}>
+      <Button
+        title="Press to Send Notification"
+        onPress={async () => {
+          await sendPushNotification();
+        }}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
